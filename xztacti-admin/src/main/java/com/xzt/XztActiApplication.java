66ab0373cb124fe2962ff2c6dbeb66fa6fd44fe4
jiejiasa <3607960766@qq.com>
2023-10-22 18:33:40 +0800

    修改

--- xztacti-admin/src/main/java/com/xzt/XztActiApplication.java
+++ xztacti-admin/src/main/java/com/xzt/XztActiApplication.java
@@ -18,15 +18,6 @@ public class XztActiApplication
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(XztActiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        System.out.println("(♥◠‿◠)ﾉﾞ  苏州行者通车辆库存系统启动成功   ლ(´ڡ`ლ)");
     }
 }
